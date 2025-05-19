335ccdf2afefe5d5de22a412416c8286c7fcdd05
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-05-19 17:50:25 +1000

    europeana api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,15 +21,27 @@ const Home = () => {
   return (
     <div>
       <h1 className="flex justify-center mb-4">Main art</h1>
-      <ul className="grid grid-rows-3 grid-cols-3 gap-10 w-auto">
+      <ul className="grid grid-rows-3 grid-cols-3 gap-6 w-auto ">
         {arts.map((art, index) => (
-          <li key={index} className="border p-2 shadow-md">
-            <h2 className="w-auto h-16">{art.title}</h2>
+          <li
+            key={index}
+            className="border rounded-2xl shadow-md bg-white transition-transform duration-300 hover:shadow-xl hover:scale-[1.03] cursor-pointer"
+          >
             <img
               src={art.edmPreview[0]}
-              alt="picture of the art piece"
-              className="h-48 w-full object-cover "
+              alt={`Image of ${art.title[0]}`}
+              className="w-full h-48 object-cover rounded-t-2xl"
             />
+            <div className="p-4 space-y-3">
+              <h2 className="text-xl font-bold text-gray-900 flex justify-center">
+                {art.title[0]}
+              </h2>
+              <p className="text-gray-700">
+                <span className="font-semibold text-blue-700">Exposed at:</span>{" "}
+                {art.dataProvider}
+              </p>
+              <p className="text-gray-600 text-sm">From: {art.country}</p>
+            </div>
           </li>
         ))}
       </ul>
--- src/services/europeanaService.js
+++ src/services/europeanaService.js
@@ -1,27 +1,23 @@
 import apiClient from "../api/apiClient";
 
-const getRandomKeyword = () => {
+const getRandomKeyWord = () => {
   const keywords = [
-    "painting",
-    "photography",
-    "sculpture",
+    "art",
+    "nature",
+    "history",
+    "science",
+    "technology",
+    "culture",
     "architecture",
-    "design",
-    "Renaissance",
-    "Impressionism",
-    "Baroque",
-    "Surrealism",
-    "Ancient",
-    "fine arts",
-    "masterpieces",
-    "art gallery",
+    "photography",
+    "music",
+    "literature",
   ];
-
   return keywords[Math.floor(Math.random() * keywords.length)];
 };
 
 export const fetchEuropeanaData = async (
-  query = getRandomKeyword(),
+  query = "art",
   language = "en",
   rows = 50
 ) => {
@@ -30,13 +26,12 @@ export const fetchEuropeanaData = async (
       params: {
         wskey: import.meta.env.VITE_API_KEY,
         query,
-        qf: "TYPE:IMAGE OR TYPE:VIDEO", // Pour plus de variété
+        qf: `TYPE:IMAGE`,
         thumbnail: true,
         media: true,
         profile: "rich",
         rows,
         language,
-        sort: "timestamp_created+desc", // Tri par date de création
       },
     });
     return response.data;
