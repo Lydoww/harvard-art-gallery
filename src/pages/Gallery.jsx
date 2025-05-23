a345306f6ac398ddae2618d126e93ee26baa41f2
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-05-23 10:28:19 +1000

    started the gallery page, fetch the general information

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { fetchGalleries } from "../services/harvardService";
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+  const [galleryArt, setGalleryArt] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default Gallery
+  const loadGalleryWork = async () => {
+    setLoading(true);
+    try {
+      const result = await fetchGalleries();
+      setGalleryArt(result.records);
+      setLoading(false);
+      console.log(result);
+    } catch (error) {
+      console.error("Error rendering gallery data:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadGalleryWork();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="flex justify-center mb-4 text-2xl font-semibold">Brower through various gallery, click to learn more about it.</h1>
+      <ul className="grid grid-rows-3 grid-cols-3 gap-6 w-auto">
+        {galleryArt.map((gallery) => (
+          <li key={gallery.galleryid}>
+            <h2>{gallery.name}</h2>
+            <p>Donor: {gallery.donorname || "unknown"}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Gallery;
--- src/services/harvardService.js
+++ src/services/harvardService.js
@@ -31,3 +31,16 @@ export const fetchArtItemById = async (id) => {
     throw error;
   }
 };
+
+export const fetchGalleries = async () => {
+  try {
+    const response = await apiClient.get(`/gallery`, {
+      params: {
+        apikey: import.meta.env.VITE_API_KEY,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching the galleries data:", error);
+  }
+};
